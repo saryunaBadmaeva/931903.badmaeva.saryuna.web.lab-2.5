19177021d0d5ca1162e0698002c7110da417a364
saryunaBadmaeva <90922702+saryunaBadmaeva@users.noreply.github.com>
2022-05-07 17:25:38 +0700

    lab-2.5

---
+++ Web_Lab5/Models/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Web_Lab5.Models;
+
+namespace Web_Lab5.Models
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Lab> Labs { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
---
+++ Web_Lab5/Models/Hospital.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Lab5.Models
+{
+    public class Hospital
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        public string Phone { get; set; }
+    }
+}
